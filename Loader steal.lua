743f37d93ce7d5695d052cdca8f3dace8a70a587
LucasggkX <iranidesouza29@gmail.com>
2025-08-21 15:14:00 -0300

    Update Loader steal.lua

--- Loader steal.lua
+++ Loader steal.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LucasggkX/Steal-A-brainrot/main/Key/Key%20ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LucasggkX/Steal-A-brainrot/refs/heads/main/Key/Key%20ui%20-%20Steal.lua"))()
