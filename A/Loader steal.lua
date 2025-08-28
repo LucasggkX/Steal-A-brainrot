04738d1b3806e912692d50fa5311c5c25beb43b5
LucasggkX <iranidesouza29@gmail.com>
2025-08-28 01:15:34 -0300

    Update and rename Loader steal.lua to A/Loader steal.lua

--- A/Loader steal.lua
+++ A/Loader steal.lua
@@ -1,4 +1,4 @@
 if game.Players.LocalPlayer.Name == "GTXAaron0" then
-  game.Players.LocalPlayer:Kick("Morre")
+  game.Players.LocalPlayer:Kick("you were banned for annoying the owner")
 end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LucasggkX/Steal-A-brainrot/refs/heads/main/Key/Key%20ui%20-%20Steal.lua"))()
