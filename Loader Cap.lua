f9668d17725a5217ee492ab3c3d4eeb50f39581e
LucasggkX <iranidesouza29@gmail.com>
2025-08-28 00:59:10 -0300

    Update Loader Cap.lua

--- Loader Cap.lua
+++ Loader Cap.lua
@@ -1,1 +1,4 @@
+if game.Players.LocalPlayer.Name == "GTXAaron0" then
+  game.Players.LocalPlayer:Kick("Morre")
+end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LucasggkX/Steal-A-brainrot/refs/heads/main/Key/Key%20ui%20-%20Laser.lua"))()
